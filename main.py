38bb612ee9292e87d25dac76938f9399d6b43e6e
kamathams0 <s6sakama@uni-bonn.de>
2021-10-31 18:01:33 +0100

    Programming task a-2, trying to sample data

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
 from load_mnist import load_mnist
+import numpy as np
+import torch as th
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-images, label = load_mnist(dataset="training", path=".")
+# Importing data in the form of tensors
+training_images, training_labels = load_mnist(dataset="training", path=".")
+test_images, test_labels = load_mnist(dataset="testing", path=".")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
