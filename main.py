c6bf20f7f5b6eb3583dc61bded1867e774e96f82
kamathams0 <s6sakama@uni-bonn.de>
2021-10-31 21:04:39 +0100

    Programming task a-2, Sampling to be done

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from load_mnist import load_mnist
 import numpy as np
-import torch as th
+import pandas as pd
+import torch
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Importing data in the form of tensors
 training_images, training_labels = load_mnist(dataset="training", path=".")
 test_images, test_labels = load_mnist(dataset="testing", path=".")
+
+# Converting tensors to numpy arrays
+training_images_arr = training_images.numpy()
+training_labels_arr = training_labels.numpy()
+
 
 
 
