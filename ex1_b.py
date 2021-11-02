cce125df1de60bae74e1527e09f2e69bf948298a
PreethamGY4848 <preethamgy44@gmail.com>
2021-11-02 22:30:06 +0100

    Programing TASK B completed

--- ex1_b.py
+++ ex1_b.py
@@ -12,30 +12,13 @@ from load_mnist import load_mnist
 import sklearn.preprocessing
 
 
-import warnings
-warnings.filterwarnings("ignore")
-
-plt.ion()
-
-
 class FMN(Dataset):
-#class FMN():
-    """Face Landmarks dataset."""
-
     def __init__(self, transform=None):
-        """
-        Args:
-            csv_file (string): Path to the csv file with annotations.
-            root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
-        """
-        # self.landmarks_frame = pd.DataFrame(label)
-        # self.root_dir = 
+ 
         self.root_dir, self.landmarks_frame = load_mnist(dataset="training", path=".")
         self.transform = transform
 
-    ######## 0 and 1 index
+    
         self.landmarks_frame = self.landmarks_frame.numpy()
         result1 = numpy.where(self.landmarks_frame == 0)
         result2 = numpy.where(self.landmarks_frame == 1)
@@ -43,93 +26,42 @@ class FMN(Dataset):
         result = result.ravel()
         result = numpy.sort(result)
         self.id = torch.tensor(result)
-        
-        #print(len(result))
-        
-        #print(idx)
-        #idx = idx.ravel()
         self.landmarks_frame = self.landmarks_frame[result]
         self.root_dir = self.root_dir[result]
-        
-        # print(self.root_dir.shape)
-        #print(self.landmarks_frame , len(self.root_dir))
 
-    #######mean and variance
+#Image scaling 
+        arr = numpy.empty((28,28))
+        for i in range(len(self.root_dir)):
+            np_array = self.root_dir[i].numpy()
+            arr1 = sklearn.preprocessing.minmax_scale(np_array, feature_range=(-1,1))
+            arr = numpy.concatenate((arr,arr1))
+        arr = arr[28:,:]   
+        arr = numpy.reshape(arr,self.root_dir.shape) 
+        self.root_dir = arr 
 
-
-        # dataset = TensorDataset(self.root_dir, self.landmarks_frame)
-        # loader = DataLoader(dataset, batch_size=8)
-
-        # nimages = 0
-        # mean = 0.
-        # std = 0.
-        # for batch, _ in loader:
-        #     # Rearrange batch to be the shape of [B, C, W * H]
-        #     batch = batch.view(batch.size(0), batch.size(1), -1)
-        #     # Update total number of images
-        #     nimages += batch.size(0)
-        #     # Compute mean and std here
-        #     mean += batch.mean(2).sum(0) 
-        #     std += batch.std(2).sum(0)
-
-        #     # Final step
-        # mean /= nimages
-        # std /= nimages
-
-        # print(mean)
-        # print(std)
-
-        # self.mean = 
-        # self.variance = 
-
-
-    ######## image scale
-        self.root_dir = sklearn.preprocessing.minmax_scale(self.root_dir, feature_range=(-1,1))
-        
-
-    ########processing step iinclude
-        
-
-
+#Function __len__
     def __len__(self):
         return len(self.landmarks_frame)
 
-
+#Function __getitem__
     def __getitem__(self, idx):
-        
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        # print("idx",idx)
-        #self.landmarks_frame = str(self.landmarks_frame[idx])
         img_name = self.landmarks_frame
-        
-        
-        #image = io.imread(img_name)
-        #print(image)
-        #landmarks = self.landmarks_frame[idx]
-        # landmarks = numpy.array([landmarks])
-        # landmarks = landmarks.astype('float').reshape(-1, 2)
-        # sample = {'image': image, 'landmarks': landmarks}
-
-        if self.transform:
-            sample = self.transform(sample)
-
         img  = self.root_dir[idx,:,:]
         label = self.landmarks_frame[idx]
         sample = {'image' : img, 'label':label}
         return sample         
 
-# #dataloader = torch.utils.data.DataLoader(dataset, batch_size=32,
-# # shuffle=True, drop_last=False)
-
-
-
+#Testing for the Dataset
 fmn = FMN(Dataset)
 
 for i in range(12000):
-
     sample = fmn[i]
     print(sample['image'].shape,sample['label'])
-    # print(sample)
-# dataloader = torch.utils.data.DataLoader(fmn, batch_size=32,
-# shuffle=True, drop_last=False)
+
+#Dataloader    
+dataloader = torch.utils.data.DataLoader(fmn, batch_size=32,shuffle=True, drop_last=False)
+
+for (idx,sample) in enumerate(dataloader):
+    print(idx,sample['image'].shape,sample['label'])
